9e60775668b4287a70e7189d0c515b621026b597
pbj <bjpark3321@gmail.com>
2023-06-01 01:31:17 +0900

    python class, module, package, decorator

---
+++ modtest.py
@@ -0,0 +1,8 @@
+# modtest.py
+
+import mod2
+
+result = mod2.add(3,4)
+print(result)
+
+# 위에서 볼 수 있듯이 다른 파이썬 파일에서도 import mod2로 mod2 모듈을 불러와서 사용할 수 있다. 대화형 인터프리터에서 한 것과 마찬가지 방법이다. 위 예제가 정상적으로 실행되기 위해서는 modtest.py 파일과 mod2.py 파일이 동일한 디렉터리(C:\doit)에 있어야 한다.
